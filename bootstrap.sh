089e67e34546526e1273014a76affc07264abd69
kressi <mkressibucher@hotmail.com>
2017-03-29 21:47:16 +0200

    Remove vimrc, has moved to seperate repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="toprc vimrc xinitrc Xmodmap Xresources zprofile zshrc"
+files="toprc xinitrc Xmodmap Xresources zprofile zshrc"
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
