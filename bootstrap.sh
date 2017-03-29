4f8b952e5a083a83ee35c226dd32906484bf89fb
kressi <mkressibucher@hotmail.com>
2017-03-29 21:30:56 +0200

    Always use olddir variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@
 #
 ####################################################
 
-dir=$HOME/dotfiles                    # dotfiles directory
-olddir=$HOME/dotfiles_old             # old dotfiles backup directory
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
 files="toprc vimrc xinitrc Xmodmap Xresources zprofile zshrc"
 
 # create dotfiles_old in homedir
@@ -24,7 +24,7 @@ echo "...done"
 # then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
